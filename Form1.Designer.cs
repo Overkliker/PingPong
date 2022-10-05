097530fe814ffa295e7e18d4b5547299ce750b78
Danil <danilahreno@yandex.ru>
2022-10-05 22:02:42 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backGround = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.platform = new System.Windows.Forms.PictureBox();
+            this.lose = new System.Windows.Forms.Timer(this.components);
+            this.backGround.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backGround
+            // 
+            this.backGround.Controls.Add(this.ball);
+            this.backGround.Controls.Add(this.platform);
+            this.backGround.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.backGround.Location = new System.Drawing.Point(0, 0);
+            this.backGround.Name = "backGround";
+            this.backGround.Size = new System.Drawing.Size(800, 450);
+            this.backGround.TabIndex = 0;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ball.Location = new System.Drawing.Point(197, 158);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(50, 50);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // platform
+            // 
+            this.platform.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.platform.Location = new System.Drawing.Point(284, 361);
+            this.platform.Name = "platform";
+            this.platform.Size = new System.Drawing.Size(155, 23);
+            this.platform.TabIndex = 0;
+            this.platform.TabStop = false;
+            // 
+            // lose
+            // 
+            this.lose.Interval = 1;
+            this.lose.Tick += new System.EventHandler(this.lose_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.backGround);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.backGround.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel backGround;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox platform;
+        private System.Windows.Forms.Timer lose;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ping
+{
+    public partial class Form1 : Form
+    {
+        public int vert = 4;
+        public int hor = 4;
+        public int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            lose.Enabled = true;
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            platform.Top = backGround.Bottom - (backGround.Bottom / 10);
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void lose_Tick(object sender, EventArgs e)
+        {
+            platform.Left = Cursor.Position.X - (platform.Width / 2);
+            ball.Left += hor;
+            ball.Top += vert;
+
+            if (ball.Left <= backGround.Left)
+            {
+                hor *= -1;
+            }
+            else if (ball.Right >= backGround.Right)
+            {
+                hor *= -1;
+            }
+            else if (ball.Top <= backGround.Top)
+            {
+                vert *= -1;
+            }
+            else if (ball.Bottom >= backGround.Bottom)
+            {
+                lose.Enabled = false;
+            }
+
+            if (ball.Bottom >= platform.Top && ball.Top <= platform.Bottom && ball.Right >= platform.Left && ball.Left <= platform.Right)
+            {
+                hor += 3;
+                vert += 3;
+                vert *= -1;
+            }
+        }
+    }
+}
